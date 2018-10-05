f4d2ca9c0749f8ddf6a46538e71b3200504468f9
AishaShrestha <aishashrestha33@gmail.com>
2018-10-04 21:00:57 -0500

    Initial Commit

---
+++ Destroy the world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Destroy the world
+//
+//  Created by Aisha Shrestha on 10/4/18.
+//  Copyright © 2018 Aisha Shrestha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
