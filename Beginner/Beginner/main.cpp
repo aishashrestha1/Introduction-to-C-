0df45a677cf6534111856836b921930daa5549f2
AishaShrestha <aishashrestha33@gmail.com>
2018-10-08 19:53:50 -0500

    Second commit

---
+++ Beginner/Beginner/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Beginner
+//
+//  Created by Aisha Shrestha on 10/8/18.
+//  Copyright © 2018 Aisha Shrestha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    
+    
+    std::cout <<"Please add your two favorite number that you want to add:\n";
+    int a,b;
+    std ::cin >>a;
+    std :: cin >> b;
+    std :: cout << a+b;
+    std :: cout << "\n";
+    
+    
+    
+    
+    //return 0;
+    
+    
+}
